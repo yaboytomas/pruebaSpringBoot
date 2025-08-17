2a1afa95f9bb3e2b079a92fc935baba572df672d
Tomas <tomasszabo94@gmail.com>
2025-08-17 11:04:18 -0400

    finished cliente controller

--- src/main/java/com/ipss/demo/controller/ClienteController.java
+++ src/main/java/com/ipss/demo/controller/ClienteController.java
@@ -1,6 +1,23 @@
 package com.ipss.demo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.ipss.demo.model.Usuario;
+
+import ch.qos.logback.core.model.Model;
+import jakarta.servlet.http.HttpSession;
+
 @Controller
 public class ClienteController {
-    
+
+    @GetMapping("/cliente/home")
+    public String homeCliente(HttpSession session, Model model) {
+        Usuario usuario = (Usuario) session.getAttribute("usuario");
+        if (usuario == null || !"Cliente".equals(usuario.getRol())) {
+            return "redirect:/login";
+        }
+        return "cliente/home";
+    }
+
 }
