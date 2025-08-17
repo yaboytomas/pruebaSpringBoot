d008e7a90bd3360e49a363021244f603cff4faad
Tomas <tomasszabo94@gmail.com>
2025-08-17 10:43:45 -0400

    created service for user and repository

---
+++ src/main/java/com/ipss/demo/repository/UsuarioRepository.java
@@ -0,0 +1,19 @@
+package com.ipss.demo.repository;
+
+import org.springframework.stereotype.*;;
+import com.ipss.demo.model.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    Optional<Usuario> findByNombre(String nombre);
+    Optional<Usuario> findByNombreAndContrasena(String nombre, String contrasena);
+
+    
+
+}
+
+
+
---
+++ src/main/java/com/ipss/demo/service/UsuarioService.java
@@ -0,0 +1,53 @@
+package com.ipss.demo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ipss.demo.model.Usuario;
+import com.ipss.demo.repository.UsuarioRepository;
+
+import java.util.Optional;
+import java.util.List;
+
+@Service
+public class UsuarioService {
+    
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    public void inicializarUsuarios() {
+
+        if (usuarioRepository.findByNombre("admin").isEmpty()) {
+            Usuario admin = new Usuario();
+            admin.setNombre("admin");
+            admin.setContrasena("admin");
+            admin.setRol("Administrator");
+            usuarioRepository.save(admin);
+        }
+
+        if (usuarioRepository.findByNombre("cliente").isEmpty()) {
+            Usuario cliente = new Usuario();
+            cliente.setNombre("cliente");
+            cliente.setContrasena("cliente");
+            cliente.setRol("Cliente");
+            usuarioRepository.save(cliente);
+        }
+
+    // Create User
+    public Optional<Usuario> registrarUsuario(Usuario nuevoUsuario) {
+        if (usuarioRepository.findByNombre(nuevoUsuario.getNombre()).isPresent()) {
+            return Optional.empty();
+        }
+        nuevoUsuario.setRol("Cliente");
+        return Optional.of(usuarioRepository.save(nuevoUsuario));
+    }
+
+    // Get User 
+    public Optional<Usuario> buscarUsuarioPorNombreYContrasena(String nombre, String contrasena) {
+        return usuarioRepository.findByNombreAndContrasena(nombre, contrasena);
+    }
+
+    // Get All Users
+    public List<Usuario> obtenerTodosLosUsuarios() {
+        return usuarioRepository.findAll();
+    }
